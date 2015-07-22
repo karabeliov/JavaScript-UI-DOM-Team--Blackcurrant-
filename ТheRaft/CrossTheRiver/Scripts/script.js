5cd800b40ffc8b2c12fc438474a05a418a9c04e1
Kristian Karabeliov <kristian@karabeliov.com>
2015-07-22 20:13:23 +0300

    add Raft and children

--- ТheRaft/CrossTheRiver/Scripts/script.js
+++ ТheRaft/CrossTheRiver/Scripts/script.js
@@ -1,19 +1,31 @@
 ﻿(function StartGame() {
-    var transferred = 0,
-        sail = 0,
-        conflict;  // conflict = all.onShore && criminalMan.onShore || boys.onShore && mother.onShore || girls.onShore && father.onShore;
+    var sail = 0,
+        rightSide = [],
+        conflict;  // conflict = all.onShore && prisoner.onShore || sons.onShore && mother.onShore || daughters.onShore && father.onShore;
 
     //  Conflict:
-    //  criminalMan 	-		with any one unless the cop is with him
-    //  girls		    -		with father unless the mother is with them
-    //  boys		    -		with mother unless the father is with them
+    //  prisoner 	-		with any one unless the cop is with him
+    //  daughters		    -		with father unless the mother is with them
+    //  sons		    -		with mother unless the father is with them
+
+    var Raft = (function (parent) {
+        var raft = Object.defineProperties(parent, {
+            side: {
+                value: left
+            },
+            speed: {    // example
+                value: 100
+            }
+        });
+        return raft;
+    }());
 
     var Person = {
         onShore: true
     }
 
     // Protector
-    var cop = (function (parent) {
+    var Cop = (function (parent) {
         var cop = Object.defineProperties(parent, {
             protector: {
                 value: true
@@ -22,7 +34,7 @@
         return cop;
     }(Person));
 
-    var father = (function (parent) {
+    var Father = (function (parent) {
         var father = Object.defineProperties(parent, {
             protector: {
                 value: true
@@ -31,7 +43,7 @@
         return father;
     }(Person));
 
-    var mother = (function (parent) {
+    var Mother = (function (parent) {
         var mother = Object.defineProperties(parent, {
             protector: {
                 value: true
@@ -40,8 +52,38 @@
         return mother;
     }(Person));
 
+
+    // Other people
+    var Prisoner = (function (parent) {
+        var prisoner = Object.defineProperties(parent, {
+
+        });
+        return prisoner;
+    }(Person));
+
+    var Children = (function (parent) {
+        var children = Object.defineProperties(parent, {
+            
+        });
+        return children;
+    }(Person));
+
+    var Son = (function (parent) {
+        var son = Object.defineProperties(parent, {
+
+        });
+        return son;
+    }(Children));
+
+    var Daughter = (function (parent) {
+        var daughter = Object.defineProperties(parent, {
+
+        });
+        return daughter;
+    }(Children));
+
     while (true) {
-        if (transferred == 8) {
+        if (rightSide.length == 8) {
             console.log('You Win! Your sail is ' + sail); // Change with Winer Screen
             break;
         }
@@ -51,24 +93,36 @@
             break;
         }
 
-        function sailing(Person) { // one or two person
-            if (false) { // is children
+        function sailing(people) { // one or two person
+            if (people instanceof Children) { // is children
                 console.log('Children can not go alone!');
             }
             else if (conflict) {
-                if (Person.protector) { 
+                if (people.protector) {
                     console.log('The Raft is GO '); // raft is go to other side
                     sail++;
-                    transferred += arguments.length;  // one or two
-                    transferred -= arguments.length;  // when is coming back one or two
+
+                    if (Raft.side == 'left') {
+                        rightSide.push(arguments.length); // one or two
+                    }
+                    else {
+                        rightSide.pop(arguments.length);  // when is coming back one or two
+                        Raft.side = 'right'
+                    }            
                 } else {
                     console.log('Have a conflict! '); // what is the conflict
                 }
             } else {
                 console.log('The Raft is GO '); // raft is go to other side
                 sail++;
-                transferred += arguments.length;  // one or two
-                transferred -= arguments.length;  // when is coming back one or two
+
+                if (Raft.side == 'left') {
+                    rightSide.push(arguments.length); // one or two
+                }
+                else {
+                    rightSide.pop(arguments.length);  // when is coming back one or two
+                    Raft.side = 'right'
+                }
             }
         }
     }
