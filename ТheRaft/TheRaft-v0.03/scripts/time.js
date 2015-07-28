3e652dab7c9d4284ab86d9071a7c6b0922141413
theshaftman <mariyan_apostolov@mail.bg>
2015-07-28 14:21:37 +0300

    A little correction

--- ТheRaft/TheRaft-v0.03/scripts/time.js
+++ ТheRaft/TheRaft-v0.03/scripts/time.js
@@ -3,16 +3,15 @@ function startTime() {
         timer = 60 * 10,
         minutes,
         seconds;
-    console.log(display);
 
     setInterval(function () {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
 
-        display.innerHTML = minutes + ":" + seconds;
+        display.innerHTML = minutes + ':' + seconds;
         //console.log(minutes + ':' + seconds);
 
         if (--timer < 0) {
